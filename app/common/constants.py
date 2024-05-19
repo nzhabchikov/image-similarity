3e1e85699621590e3d95968046c5e7ccef371de7
nzhabchikov <nikolay.zhabchikov@gmail.com>
2024-05-19 23:59:25 +0300

    replaced autoencoder to cnn model

--- app/common/constants.py
+++ app/common/constants.py
@@ -1,10 +1,16 @@
+from enum import Enum
+
 from torch import device, cuda
 
 TEMP_PATH = '/temp'
 SAVED_MODELS_PATH = 'app/models/saved'
-AUTOENCODER_PATH = SAVED_MODELS_PATH + '/autoencoder_dict.pt'
-FINE_TUNED_AUTOENCODER_PATH = SAVED_MODELS_PATH + '/fine_tuned_autoencoder_dict.pt'
-NEAREST_NEIGHBOR_PATH = SAVED_MODELS_PATH + '/nearest_neighbors.pkl'
+NEAREST_NEIGHBORS = 'nearest_neighbors'
 DEVICE = device('cuda' if cuda.is_available() else 'cpu')
 DEFAULT_BATCH_SIZE = 32
 DEFAULT_N_NEIGHBORS = 5
+DEFAULT_RESIZE_HEIGHT, DEFAULT_RESIZE_WIDTH = 192, 192
+
+
+class ModelType(Enum):
+    standard = 'standard'
+    lite = 'lite'
